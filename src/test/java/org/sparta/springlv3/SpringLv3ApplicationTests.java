cdfdc4c2fe69fbc6a98f37da0331d77f568998f1
Seungwon_Choi <bestwondev@gmail.com>
2024-07-11 12:49:04 +0900

    initial commit

---
+++ src/test/java/org/sparta/springlv3/SpringLv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.springlv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
